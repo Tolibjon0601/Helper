aafc88f30cfa3b692150707d256f155d20ff858e
Tolibjon0601 <egamberdiyevtolibjon0601@gmail.com>
2025-09-04 13:26:56 +0500

    fix

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -56,8 +56,8 @@
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({
-            email: "isbd-sayt",
-            password: "123456"         }),
+            email: "helper-sayt",
+            password: "12345"         }),
         })
           .then((res) => {
             if (!res.ok) throw new Error("Login xatosi");
